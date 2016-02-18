ff56c18822c89553f94a9065dae78e880f73403b
Pawanjit <pawanjit.nayyar@gmail.com>
2016-02-18 14:31:26 -0800

    Added some files

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.Linq;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        StudentsDatabaseDataContext studentDBC = new StudentsDatabaseDataContext();
+        var allstudents = from MyStudent in studentDBC.Students where MyStudent.StudentID > 1 select MyStudent;
 
+        GridView1.DataSource = allstudents;
+        GridView1.DataBind();
     }
 }
